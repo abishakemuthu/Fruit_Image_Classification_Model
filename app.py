4ab7957337a761e08e9589919cd2b2567cc50304
abishake <abishakemuthu@gmail.com>
2024-10-17 12:37:48 +0530

    updated

--- app.py
+++ app.py
@@ -6,21 +6,22 @@ import numpy as np
 
 st.header('Image Classification Model')
 
-model = load_model("C:\\Users\\Abi\\Documents\\ML Projects\\Image Classification Model\\Image_classification_model.keras")
+model = load_model("Image_classification_model.h5")
 
 cat = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon']
 
-image = st.text_input('Enter the Image Name ','Apple.jpg')
+image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-image_load = tf.keras.utils.load_img(image,target_size=(180,180))
-ima_arr = tf.keras.utils.array_to_img(image_load)
-img_bat = tf.expand_dims(ima_arr,0)
+if image:
+    image_load = tf.keras.utils.load_img(image,target_size=(180,180))
+    ima_arr = tf.keras.utils.array_to_img(image_load)
+    img_bat = tf.expand_dims(ima_arr,0)
 
-pred = model.predict(img_bat)
+    pred = model.predict(img_bat)
 
-score = tf.nn.softmax(pred)
+    score = tf.nn.softmax(pred)
 
-st.image(image,width=200)
+    st.image(image,width=200)
 
-st.write('Veg/Fruit in image is ' , cat[np.argmax(score)])
-st.write('The Accuracy is ' , round(np.max(score)*100,2))
+    st.write('Veg/Fruit in image is ' , cat[np.argmax(score)])
+    st.write('The Accuracy is ' , round(np.max(score)*100,2))
